91659a89c9e5738fca29c1a3ff4c1882f778c65c
finn7199 <80686906+finn7199@users.noreply.github.com>
2023-02-04 03:14:56 +0530

    UI added

---
+++ Assets/Game/Scripts/UIFunctions.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
