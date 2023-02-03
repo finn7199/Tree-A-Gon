2a63264af1c91363aaf4799b91805c5bd9ff8827
finn7199 <80686906+finn7199@users.noreply.github.com>
2023-02-04 01:13:10 +0530

    Script added

---
+++ Assets/Game/Scripts/Hexagon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    [SerializeField] GameObject top;
+    [SerializeField] GameObject topLeft;
+    [SerializeField] GameObject topRight;
+    [SerializeField] GameObject bottom;
+    [SerializeField] GameObject bottomLeft;
+    [SerializeField] GameObject bottomRight;
+}
