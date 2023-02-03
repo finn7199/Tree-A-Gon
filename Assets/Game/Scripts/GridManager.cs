300cdfcfeb236cd995a369d057535d32abd6f4e8
BongoMASK <67361918+BongoMASK@users.noreply.github.com>
2023-02-04 04:16:39 +0530

    Created grid system

---
+++ Assets/Game/Scripts/GridManager.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public static GridManager Instance;
+
+    [SerializeField] int _width, _height;
+    [SerializeField] Hexagon _tilePrefab;
+    [SerializeField] Transform tileParent;
+
+    Dictionary<Vector2, Hexagon> tiles = new Dictionary<Vector2, Hexagon>();
+
+    // Spacing values
+    public static float xDist = 1.5f;
+    public static float yDist = 1.3f;
+
+    void Start()
+    {
+        Instance = this;
+        GenerateGrid();
+    }
+
+    /// <summary>
+    /// Generates a gridth as per width and height
+    /// </summary>
+    void GenerateGrid() {
+
+        // Positioning the tiles
+        for (int x = 0; x < _width; x++) {
+            for (int y = 0; y < _height; y++) {
+
+                var spawnedTile = Instantiate(_tilePrefab);
+
+                spawnedTile.transform.position = new Vector3(x * xDist, y * yDist);
+
+                if (y % 2 == 0)
+                    spawnedTile.transform.position += new Vector3(xDist / 2, 0);
+
+                spawnedTile.transform.parent = tileParent;
+                spawnedTile.name = $"{x} {y}";
+                tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+
+        // Assigning data for borders
+        foreach (var key in tiles.Keys)
+            tiles[key].SetDirectionData();
+    }
+
+    /// <summary>
+    /// Finds tile by 2D Index of tile
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    public Hexagon GetTileByIndex(Vector2 pos) {
+        if (tiles.ContainsKey(pos))
+            return tiles[pos];
+        return null;
+    }
+}
--- Assets/Game/Scripts/Hexagon.cs
+++ Assets/Game/Scripts/Hexagon.cs
@@ -1,11 +1,57 @@
+using System;
 using UnityEngine;
 
 public class Hexagon : MonoBehaviour
 {
-    [SerializeField] GameObject top;
-    [SerializeField] GameObject topLeft;
-    [SerializeField] GameObject topRight;
-    [SerializeField] GameObject bottom;
-    [SerializeField] GameObject bottomLeft;
-    [SerializeField] GameObject bottomRight;
+
+    /// <summary>
+    /// Border possible positions
+    /// </summary>
+    public enum TileBorder {
+        topLeft=0,
+        topRight,
+        right,
+
+        downLeft,
+        downRight,
+        left
+    }
+
+    public bool isOccupied = false;
+
+    public Hexagon[] borders;
+
+    /// <summary>
+    /// Finds borders by using index
+    /// </summary>
+    public void SetDirectionData() {
+        int x = Int32.Parse(name.Split(' ')[0]);
+        int y = Int32.Parse(name.Split(' ')[1]);
+
+        borders[(int)TileBorder.topLeft] = GridManager.Instance.GetTileByIndex(new Vector2(x, y + 1));     // top left
+        borders[(int)TileBorder.topRight] = GridManager.Instance.GetTileByIndex(new Vector2(x + 1, y + 1));     // top right
+        borders[(int)TileBorder.right] = GridManager.Instance.GetTileByIndex(new Vector2(x + 1, y));     // right
+
+        borders[(int)TileBorder.downLeft] = GridManager.Instance.GetTileByIndex(new Vector2(x, y - 1));     // down left
+        borders[(int)TileBorder.downRight] = GridManager.Instance.GetTileByIndex(new Vector2(x + 1, y - 1));     // down right
+        borders[(int)TileBorder.left] = GridManager.Instance.GetTileByIndex(new Vector2(x - 1, y));     // left
+    }
+
+    /// <summary>
+    /// Finds borders by using position of tile
+    /// </summary>
+    public void SetDirectionDataByPosition() {
+        Vector2 tilePos = transform.position;
+
+        float x = GridManager.xDist;
+        float y = GridManager.yDist;
+
+        borders[(int)TileBorder.topLeft] = GridManager.Instance.GetTileByIndex(tilePos + new Vector2(-x / 2, y));     // top left
+        borders[(int)TileBorder.topRight] = GridManager.Instance.GetTileByIndex(tilePos + new Vector2(x / 2, y));     // top right
+        borders[(int)TileBorder.right] = GridManager.Instance.GetTileByIndex(tilePos + new Vector2(x, 0));     // right
+
+        borders[(int)TileBorder.downLeft] = GridManager.Instance.GetTileByIndex(tilePos + new Vector2(-x / 2, -y));     // down left
+        borders[(int)TileBorder.downRight] = GridManager.Instance.GetTileByIndex(tilePos + new Vector2(x / 2, -y));     // down right
+        borders[(int)TileBorder.left] = GridManager.Instance.GetTileByIndex(tilePos + new Vector2(-x, 0));     // left
+    }
 }
