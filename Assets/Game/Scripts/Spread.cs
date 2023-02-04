7ba23702b754061e3b7ef2386e46fe133b256065
BongoMASK <67361918+BongoMASK@users.noreply.github.com>
2023-02-04 18:00:19 +0530

    made tree generation good

--- Assets/Game/Scripts/Spread.cs
+++ Assets/Game/Scripts/Spread.cs
@@ -1,32 +1,30 @@
-using System.Collections;
 using UnityEngine;
 
-public class Spread : MonoBehaviour
-{
+public class Spread : MonoBehaviour {
     public Hexagon hexagon;
     public GameObject tree;
 
-
-    private void LateUpdate()
-    {
-        StartCoroutine(SpreadFunc());
+    private void Start() {
+        Invoke(nameof(SpreadFunc), 5);
     }
 
-    IEnumerator SpreadFunc()
-    {
-        yield return new WaitForSeconds(5.0f);
-        if (hexagon.isOccupied)
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                if (!hexagon.borders[i].isOccupied && !hexagon.borders[i].planted)
-                {
+    void SpreadFunc() {
+        if (hexagon.isOccupied) {
+            for (int i = 0; i < 6; i++) {
+                if (!hexagon.borders[i].isOccupied && !hexagon.borders[i].planted) {
                     Instantiate(tree, hexagon.borders[i].transform.position + new Vector3(0, 2, 0), Quaternion.identity);
                     hexagon.borders[i].planted = true;
                     hexagon.borders[i].isOccupied = true;
+                    hexagon.borders[i].GetComponent<Spread>().enabled = true;
+
+                    break; 
                 }
             }
         }
+
+        float rand = Random.Range(-100, 100);
+        rand /= 100;
+
+        Invoke(nameof(SpreadFunc), 5 + rand * 2);
     }
-    
 }
