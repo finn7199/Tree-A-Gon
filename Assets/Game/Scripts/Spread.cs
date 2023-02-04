85d509299e8d1effb324307511b7c6403057854f
finn7199 <80686906+finn7199@users.noreply.github.com>
2023-02-04 17:37:49 +0530

    spread code

--- Assets/Game/Scripts/Spread.cs
+++ Assets/Game/Scripts/Spread.cs
@@ -1,6 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spread : MonoBehaviour
 {
+    public Hexagon hexagon;
+    public GameObject tree;
+
+
+    private void LateUpdate()
+    {
+        StartCoroutine(SpreadFunc());
+    }
+
+    IEnumerator SpreadFunc()
+    {
+        yield return new WaitForSeconds(5.0f);
+        if (hexagon.isOccupied)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (!hexagon.borders[i].isOccupied && !hexagon.borders[i].planted)
+                {
+                    Instantiate(tree, hexagon.borders[i].transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+                    hexagon.borders[i].planted = true;
+                    hexagon.borders[i].isOccupied = true;
+                }
+            }
+        }
+    }
     
 }
