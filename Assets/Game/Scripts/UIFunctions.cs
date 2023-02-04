eaa88c78dfd71424d523a33f1d9aa3a4c8ca2918
finn7199 <80686906+finn7199@users.noreply.github.com>
2023-02-05 05:09:43 +0530

    next level ui/particles

--- Assets/Game/Scripts/UIFunctions.cs
+++ Assets/Game/Scripts/UIFunctions.cs
@@ -11,4 +11,8 @@ public class UIFunctions : MonoBehaviour
     {
         Application.Quit();
     }
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
